78b2cf77060d541d5d37a9fe1a093d17c1093525
tdfm3 <harutti.is.called@gmail.com>
2025-03-19 15:27:58 +0900

    codeから変更

---
+++ vsts2019.bat
@@ -0,0 +1,27 @@
+﻿@echo off
+setlocal EnableDelayedExpansion
+
+:: UAC対策（管理者として実行）
+cd /D "%~dp0"
+
+:: VSTSのパスを通す
+IF "%PROCESSOR_ARCHITECTURE%"=="x86" (
+    :: 環境変数を組み込む
+    call "%ProgramFiles%\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+) ELSE (
+    :: 環境変数を組み込む
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+)
+echo on
+title %~nx0
+
+rem サンプル１
+set SAMPLE1="あああ"
+echo %SAMPLE1%
+
+set SAMPLE2="いいい"
+echo %SAMPLE2%
+
+pause:
+
+:: vim:tw=0:
